24a6d3f26983575243803288eafe3c683fad1078
Janista09 <6306021611035@fitm.kmutnb.ac.th>
2022-04-26 02:47:06 +0700

    1.0.3

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://admin:RYArpb11146@node31370-contact-js.app.ruk-com.cloud:11256/admin')
+const db = mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/admin')
     .then(() => {
         console.log('connect success')
     })
