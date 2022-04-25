ff0a919dec58c3d46898cf69529097c9efff886b
Janista09 <6306021611035@fitm.kmutnb.ac.th>
2022-04-26 01:53:24 +0700

    update database

--- database.js
+++ database.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/admin';
-
-const db = mongoose.connect(url);
-
-mongoose.connection.on('error',(err)=>{
-    console.log(err);
-});
-
-mongoose.connection.once('open',() =>{
-    console.log("[DB] Connecting Success");
-});
+const db = mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/admin')
+    .then(() => {
+        console.log('connect success')
+    })
+    .catch((err) => {
+        console.log('connect error -> ', err);
+    })
 
 module.exports = db;
